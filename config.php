91c40b5a6a29187c6bb5b2bada48a7d23b7f3d60
FlashingChen2024 <flashingchen@gmail.com>
2025-07-13 13:30:14 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ define('DB_PASS', 'test_flashingche');
 define('DB_NAME', 'test_flashingche');
 
 // 管理员配置
-define('ADMIN_USER', 'FlashingChen');
-define('ADMIN_PASS', md5('evan520robby')); // 使用MD5加密存储密码
+define('ADMIN_USER', 'your_name');
+define('ADMIN_PASS', md5('your_pass')); // 使用MD5加密存储密码
 ?>
